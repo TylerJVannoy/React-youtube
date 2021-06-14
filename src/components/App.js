47231c46ccaec02bcb2ddba9d2fdb17dbeffd480
Tyler Vannoy <61971482+TylerJVannoy@users.noreply.github.com>
2021-06-14 16:39:05 -0700

    refactor app.js and add custom hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SearchBar from "./SearchBar";
-import youtube from "./apis/youtube";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
-import "./App.css";
+import useVideos from "../hooks/useVideos";
 
-class App extends React.Component {
-  state = { videos: [], selectedVideo: null };
+const App = () => {
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  const [videos, search] = useVideos("gabe the dog");
 
-  onTermSubmit = async term => {
-    const response = await youtube.get("/search", {
-      params: {
-        q: term,
-      },
-    });
-    this.setState({
-      videos: response.data.items,
-      selectedVideo: response.data.items[0],
-    });
-  };
+  useEffect(() => {
+    setSelectedVideo(videos[0]);
+  }, [videos]);
 
-  onVideoSelect = video => {
-    this.setState({ selectedVideo: video });
-  };
-
-  componentDidMount() {
-    this.onTermSubmit("gabe the dog");
-  }
-
-  //follow onVideoSelect, it is defined here and passed into the render() function as a prop on VideoList, move to VideoList.js now
-
-  render() {
-    return (
-      <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
-        <div className="ui grid">
-          <div className="ui row">
-            <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo} />
-            </div>
-            <div className="five wide column">
-              <VideoList
-                onVideoSelect={this.onVideoSelect}
-                videos={this.state.videos}
-              />
-            </div>
+  return (
+    <div className="ui container">
+      <SearchBar onFormSubmit={search} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList onVideoSelect={setSelectedVideo} videos={videos} />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+//follow onVideoSelect, it is defined here and passed into the render() function as a prop on VideoList, move to VideoList.js now
 
 export default App;
