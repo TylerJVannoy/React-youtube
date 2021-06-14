754b09f2db89ea92732489facb717974163c3b14
Tyler Vannoy <61971482+TylerJVannoy@users.noreply.github.com>
2021-06-14 15:48:30 -0700

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,55 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+import youtube from "./apis/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
+import "./App.css";
+
+class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
+
+  onTermSubmit = async term => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
+
+  onVideoSelect = video => {
+    this.setState({ selectedVideo: video });
+  };
+
+  componentDidMount() {
+    this.onTermSubmit("gabe the dog");
+  }
+
+  //follow onVideoSelect, it is defined here and passed into the render() function as a prop on VideoList, move to VideoList.js now
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
